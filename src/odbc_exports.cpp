727b4bd7124ffacab8c7fecace7c8f80e80d2d7c
Igor Cotruta <hugoberry314@gmail.com>
2025-04-07 23:41:58 +0100

    Use appropriate export attributes

--- src/odbc_exports.cpp
+++ src/odbc_exports.cpp
@@ -4,13 +4,20 @@
 // Define the required symbols that seem to be missing
 // These would normally be exported by the ODBC driver manager
 extern "C" {
-    // Define the odbc_version symbol - ODBC version 3.80
+    #ifdef _WIN32
+    __declspec(dllexport)
+    #else
+    __attribute__((visibility("default")))
+    #endif
     int odbc_version = 0x0380;
     
-    // Define a dummy odbc_init function
+    #ifdef _WIN32
+    __declspec(dllexport)
+    #else
+    __attribute__((visibility("default")))
+    #endif
     void odbc_init(void) {
-        // This function is typically called during ODBC initialization
-        // For our purposes, an empty implementation should be sufficient
+        // Implementation
     }
 }
 
