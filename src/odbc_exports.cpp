7097294bacd37ba9a832caa2838b6e7ecd003e03
Igor Cotruta <hugoberry314@gmail.com>
2025-04-07 23:52:17 +0100

    target apple silicon

--- src/odbc_exports.cpp
+++ src/odbc_exports.cpp
@@ -3,21 +3,20 @@
 
 // Define the required symbols that seem to be missing
 // These would normally be exported by the ODBC driver manager
+#if defined(_WIN32)
+  #define ODBC_EXPORT __declspec(dllexport)
+#else
+  #define ODBC_EXPORT __attribute__((visibility("default")))
+#endif
+
 extern "C" {
-    #ifdef _WIN32
-    __declspec(dllexport)
-    #else
-    __attribute__((visibility("default")))
-    #endif
-    int odbc_version = 0x0380;
+    // Define the odbc_version symbol - ODBC version 3.80
+    ODBC_EXPORT int odbc_version = 0x0380;
     
-    #ifdef _WIN32
-    __declspec(dllexport)
-    #else
-    __attribute__((visibility("default")))
-    #endif
-    void odbc_init(void) {
-        // Implementation
+    // Define a dummy odbc_init function
+    ODBC_EXPORT void odbc_init(void) {
+        // This function is typically called during ODBC initialization
+        // For our purposes, an empty implementation should be sufficient
     }
 }
 
