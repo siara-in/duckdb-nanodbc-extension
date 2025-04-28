f1c9ae64b2e226631eb47494652f61a6c5b328c9
Igor Cotruta <hugoberry314@gmail.com>
2025-04-28 22:20:47 +0100

    rename extension to nanodbc

--- src/include/nanodbc_extension.hpp
+++ src/include/nanodbc_extension.hpp
@@ -4,15 +4,15 @@
 
 namespace duckdb {
 
-class OdbcExtension : public Extension {
+class NanodbcExtension : public Extension {
 public:
     void Load(DuckDB &db) override;
     std::string Name() override {
-        return "odbc";
+        return "nanodbc";
     }
     std::string Version() const override {
-#ifdef EXT_VERSION_ODBC
-        return EXT_VERSION_ODBC;
+#ifdef EXT_VERSION_NANODBC
+        return EXT_VERSION_NANODBC;
 #else
         return "";
 #endif
