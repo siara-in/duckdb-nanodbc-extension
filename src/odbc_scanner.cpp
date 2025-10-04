56b87c0ed983f801e5bd7bfa85335d664f6ef2c7
Arun <arun@siara.in>
2025-10-04 15:00:40 +0530

    odbc_connect()

--- src/odbc_scanner.cpp
+++ src/odbc_scanner.cpp
@@ -192,10 +192,12 @@ unique_ptr<FunctionData> BindOdbcFunction(ClientContext &context, TableFunctionB
             int conn_idx = -1;
             std::string conn_pfx = "conn_idx=";
             auto s = params.connection.GetConnectionString();
+            printf("cs: %s\n", s.c_str());
             if (!params.connection.IsDsn() && s.rfind(conn_pfx, 0) == 0) {
                 is_already_connected = true;
                 std::string numberPart = s.substr(conn_pfx.size());
                 conn_idx = std::stoi(numberPart);
+                printf("ci: %d\n", conn_idx);
                 if (conn_idx >= odbc_connections.size()) {
                     is_already_connected = false;
                 }
